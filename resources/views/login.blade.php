6aae930af0c073bacb65633369bade73e80a1d0c
mamunapsis <al.mamun@apsissolutions.com>
2020-02-03 12:38:25 +0600

    log in page modified

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,8 +25,8 @@
                 <h1 class="logo-name">IN+</h1>
 
             </div>
-            <h3>Welcome to IN+</h3>
-            <p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
+            <h3>Welcome to Apsis Solutions</h3>
+            <p>
                 <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
             </p>
             <p>Login in. To see it in action.</p>
@@ -43,7 +43,7 @@
                 <p class="text-muted text-center"><small>Do not have an account?</small></p>
                 <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
             </form>
-            <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
+            <p class="m-t"> <small>Apsis Solutions Ltd &copy; 2020</small> </p>
         </div>
     </div>
 
